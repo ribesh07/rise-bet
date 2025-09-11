f0e690f8767bad59d441e693b2266826db77ca24
Ribesh Kumar Sah <103181197+ribesh07@users.noreply.github.com>
2025-09-12 00:34:15 +0545

    Update logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 flex items-center justify-between h-16">
         {/* Logo */}
         <div className="flex-shrink-0 font-extrabold tracking-wide cursor-pointer select-none">
-          <img className="w-8 h-8" src="logo.png" alt="RISE"/>
+          <img src="logo.png" alt="RISE"/>
         </div>
 
         {/* Desktop Nav Links */}
